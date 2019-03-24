d75b64774c29d8dec5fe763321507dbcc1eef51e
bayra <berkantbayraktar06@gmail.com>
2019-03-24 23:14:14 +0300

    Update simulation runner

--- src/SimulationRunner.java
+++ src/SimulationRunner.java
@@ -11,16 +11,10 @@ public class SimulationRunner {
         SimulationController simulation = new SimulationController(50, 50);
         SimulationObject obj1 = new RegularSoldier("Soldier1", new Position(10, 10));
         SimulationObject obj2 = new RegularZombie("Zombie1", new Position(40, 40));
-        SimulationObject obj3 = new RegularZombie("Zombie2", new Position(30, 40));
-        SimulationObject obj4 = new RegularZombie("Zombie3", new Position(30, 30));
-
-        //simulation.addSimulationObject(new RegularSoldier("Soldier1", new Position(10, 10)));
-       // simulation.addSimulationObject(new RegularZombie("Zombie1", new Position(40, 40)));
 
         simulation.addSimulationObject(obj1);
         simulation.addSimulationObject(obj2);
-        simulation.addSimulationObject(obj3);
-        simulation.addSimulationObject(obj4);
+
         //System.out.println(simulation);
         while (!simulation.isFinished()) {
             simulation.stepAll();
