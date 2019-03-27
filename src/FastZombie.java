31e752ad9269fe838b600776985f04b8a5380a6b
bayra <berkantbayraktar06@gmail.com>
2019-03-27 16:07:15 +0300

    implement slow zombie

--- src/FastZombie.java
+++ src/FastZombie.java
@@ -10,10 +10,20 @@ public class FastZombie extends Zombie {
 
     @Override
     public void step(SimulationController controller) {
+        double distance_to_closest_soldier = calculateDistanceToEnemy(controller);
+        SimulationObject closest_soldier = nearestEnemy(controller);
+
+        if(distance_to_closest_soldier <= this.getCollision_range() + ((Soldier)closest_soldier).getCollision_range()){
+            System.out.println(this.getName() + " killed " + closest_soldier.getName() + ".");
+            controller.removeSimulationObject(closest_soldier);
+            return;
+        }
+
         if(this.isFirstStep()){  // SET RANDOM DIRECTION IF THIS IS THE FIRST STEP CALL
             this.setRandomDirection();
             this.setFirstStep(false);
         }
+
     }
 
 }
--- src/SimulationRunner.java
+++ src/SimulationRunner.java
@@ -11,18 +11,14 @@ public class SimulationRunner {
         SimulationController simulation = new SimulationController(50, 50);
         SimulationObject obj1 = new RegularSoldier("RegularSoldier", new Position(10, 10));
         SimulationObject obj2 = new RegularZombie("Zombie1", new Position(40, 40));
-        //SimulationObject obj3 = new Commando("Commando", new Position(30, 30));
-        //SimulationObject obj4 = new Sniper("Sniper", new Position(30, 30));
 
         simulation.addSimulationObject(obj1);
         simulation.addSimulationObject(obj2);
-       // simulation.addSimulationObject(obj3);
-        //simulation.addSimulationObject(obj4);
+
 
         //System.out.println(simulation);
         while (!simulation.isFinished()) {
             simulation.stepAll();
-            //System.out.println(simulation);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
