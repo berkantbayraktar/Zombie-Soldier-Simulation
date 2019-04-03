e6e6d318f7e3e48b2d5a8c879c24505e7333ae0a
bayra <berkantbayraktar06@gmail.com>
2019-04-03 23:48:34 +0300

    add Javadoc comments for SoldierState, SoldierType, ZombieState and ZombieType

--- src/ZombieState.java
+++ src/ZombieState.java
@@ -1,6 +1,7 @@
 /**
- *
- *
+ * Enumeration to represent the two states a zombie can be in.
+ * @author Berkant Bayraktar
+ * @version 1.0
  */
 public enum ZombieState {
     WANDERING,
--- src/ZombieType.java
+++ src/ZombieType.java
@@ -1,6 +1,7 @@
 /**
- *
- *
+ * Enumeration to represent the types of a zombie.
+ * @author Berkant Bayraktar
+ * @version 1.0
  */
 public enum ZombieType {
     SLOW,
