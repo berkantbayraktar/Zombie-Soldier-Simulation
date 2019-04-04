4a775d77e134d65bd012c2df440d46de2e3711ac
bayra <berkantbayraktar06@gmail.com>
2019-04-05 00:10:24 +0300

    JavaDoc for Regular Soldier

--- src/RegularSoldier.java
+++ src/RegularSoldier.java
@@ -1,12 +1,61 @@
 /**
- *
- *
+ * <p> <b>RegularSoldier </b>, derived from Soldier Class, represents the regular soldiers in the simulation. </p>
+ * @author Berkant Bayraktar
+ * @version 1.0
  */
 public class RegularSoldier extends  Soldier{
+    /**
+     * <p> Creates RegularSoldier object with the given parameters. </p>
+     * <p> Name and position of the soldier are given to the parent class's constructor as parameter.</p>
+     * <p> Other values which specific to the Regular Soldier type is also given to parent class's constructor as parameter. </p>
+     * <p> Values specific to the Regular Soldier type are given below</p>
+     * <ul>
+     *     <li> {@link SoldierState}: SEARCHING </li>
+     *     <li> {@link SoldierType}: REGULAR</li>
+     *     <li> collision range of the Soldier: 2.0</li>
+     *     <li> shooting range of the Soldier: 20.0</li>
+     *     <li> speed of the Soldier: 5.0 </li>
+     * </ul>
+     * @param name Name of the Regular Soldier
+     * @param position Position of the Regular Soldier
+     */
 
     public RegularSoldier(String name, Position position) { // DO NOT CHANGE PARAMETERS
         super(name, position, 5.0,SoldierType.REGULAR, 2.0, 20.0, SoldierState.SEARCHING);
     }
+
+    /**
+     * <p> RegularSoldier object uses this method to be able to act in the simulation. </p>
+     * <p> RegularSoldier object can be in three different state which are {@link SoldierState#SEARCHING}, {@link SoldierState#AIMING}, {@link SoldierState#SHOOTING}  </p>
+     * <p> The instructions according to the states are given below.</p>
+     * <p> State: SEARCHING </p>
+     * <ul>
+     *     <li> If this is the first step call, selects soldier direction randomly.</li>
+     *     <li> Calculates the next position of the soldier with formula: new_position = position + direction * speed </li>
+     *     <li> If the position is out of bounds, changes direction to random value. </li>
+     *     <li> If the position is not out of bounds, changes soldier position to the new_position. </li>
+     *     <li> Calculates the euclidean distance to the closest zombie.</li>
+     *     <li> If the distance is shorter than or equal to the shooting range of the soldier, changes state to
+     * AIMING. </li>
+     * </ul>
+     * <p> State: AIMING </p>
+     * <ul>
+     *     <li> Calculates the euclidean distance to the closest zombie.</li>
+     *     <li> If the distance is shorter than or equal to the shooting range of the soldier, changes soldier
+     * direction to zombie and changes state to SHOOTING.</li>
+     *     <li> If not, changes state to SEARCHING.</li>
+     * </ul>
+     * <p> State: SHOOTING </p>
+     * <ul>
+     *     <li> Creates a bullet with the position and direction same as soldier's. </li>
+     *     <li> Calculates the euclidean distance to the closest zombie. </li>
+     *     <li> If the distance is shorter than or equal to the shooting range of the soldier, changes state to
+     * AIMING.</li>
+     *     <li> If not, randomly changes soldier direction and change state to SEARCHING. </li>
+     * </ul>
+     *
+     * @param controller Simulation Controller
+     */
 
     @Override
     public void step(SimulationController controller) {
